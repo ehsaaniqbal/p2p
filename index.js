7a20022b25e47570ac3293df29d21b5973ad9ff0
Ehsaan Iqbal <ishu283@gmail.com>
2019-06-28 09:10:17 +0400

    p2p-chat alpha

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = 8000 || process.env.PORT;
+
 
 app.get('/', (req, res )=>{
     res.sendFile(path.join(__dirname+'/index.html'));
@@ -10,6 +10,7 @@ app.get('/', (req, res )=>{
 
 app.use('/resources', express.static('./source'));
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
-    console.log(`Server started at ${PORT}`)
-})
+    console.log(`Server running on ${PORT}`)
+});
